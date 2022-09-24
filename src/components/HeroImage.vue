2cb3363a14aa685e6e7b50186f3395e9d88202ee
Will <wsg.online@gmail.com>
2022-09-24 19:12:34 -0300

    updated readme

--- src/components/HeroImage.vue
+++ src/components/HeroImage.vue
@@ -3,8 +3,7 @@
     <section class="bg-gray-100">
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="animate__animated animate__fadeInLeft mr-auto place-self-center lg:col-span-7">
-                <h1
-                    class=" max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
+                <h1 class=" max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
                     Ushuaia ~uˈswaʝa</h1>
                 <p class="max-w-2xl mb-6 text-gray-500 lg:mb-8 md:text-lg lg:text-xl">
                     Capital da província da Terra do Fogo, Antártida e Ilhas do Atlântico Sul.</p>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      display: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [],
 };
