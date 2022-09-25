d478dc03e2a0e5610bcc86cbf5f8db1f54b9bb3e
sanossuke <andreygustavo.oliveirasilva@gmail.com>
2022-09-24 21:22:04 -0300

    Project Finished

--- src/views/Geografia.vue
+++ src/views/Geografia.vue
@@ -1,5 +1,62 @@
 <template>
-    <h1 class="text-red-900"> Estou na Geografia
-  
-    </h1>
-  </template>
+  <div class="grid grid-cols-2 mt-10">
+
+    <div class="pr-20">
+      <p
+        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+        O nome Ushuaia é uma combinação de duas palavras do idioma yagan: ushu, um termo de difícil tradução com o
+        significado de algo que penetra amplamente em direção ao oeste, e aia, baía. O resultado pode ser interpretado
+        como "baía que penetra fundo terra adentro na direção oeste". A palavra Ushuaia apareceu por escrito pela
+        primeira vez em 11 de maio de 1869, nas atas da missão anglicana, na forma Ushu-waia. No século XIX, a palavra
+        foi grafada em até 19 variações diferentes, como Ooshovia, Usohaia, Ooshwia e outras.
+      </p>
+
+      <p
+        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+        Ushuaia encontra-se nas coordenadas 54° 48' 57" S 68° 19' 04" O, a 3 mil km de Buenos Aires, a capital
+        argentina. Está a sudoeste da Ilha Grande da Terra do Fogo, aninhada entre as encostas do cerro Martial e do
+        monte Olívia e debruçando-se sobre a Baía de Ushuaia, no limite setentrional do canal de Beagle. É a capital
+        da
+        província da Terra do Fogo, Antártica e Ilhas do Atlântico Sul, e capital e o único município do Departamento
+        Ushuaia. O departamento e, por conseguinte, o município, possui uma área de 9.017 km², incluindo a Ilha dos
+        Estados e alguns ilhotes no canal de Beagle.
+      </p>
+      <div>
+        <p
+          class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+          A cidade ocupa um dos três ambientes geográficos distintos encontrados na ilha, caracterizando-se pela
+          presença
+          dos últimos trechos da cordilheira dos Andes, que na região se estende de oeste a leste e se subdivide em
+          diversos cordões montanhosos secundários, que assumem nomes variados, como cordilheira Valdivieso e
+          cordilheira
+          Martial. Aqui eles são mais baixos do que na parte continental da cordilheira dos Andes, possuindo alturas que
+          não ultrapassam os 1 500m. Seu ponto culminante é o cerro Cornú, com 1 490 m, e a leste da zona urbana surge o
+          monte Olívia, com 1 328 m. Suas montanhas foram modeladas pelas glaciações, formando vales de leito
+          arredondado
+          e com depósitos de morenas. Ainda existem diversos glaciares, tais como o glaciar Martial, responsável por
+          fornecer a maior parte da água potável utilizada na cidade.
+
+          Inúmeros riachos sulcam esses vales glaciais e correm pelas encostas formando muitas cachoeiras; todos os
+          cursos
+          d'água são curtos e de reduzido caudal. Existem grandes lagos, destacando-se o lago Escondido e sobretudo o
+          lago
+          Fagnano. O litoral é dinamizado pela presença de muitas baías, ilhotas e fiordes. Ao sul encontram-se as águas
+          do canal de Beagle e a leste o departamento se limita pelo oceano Atlântico. A zona urbana
+          de Ushuaia está próxima da falha de Magalhães-Fagnano, que atravessa a ilha de oeste a leste e tem atividade
+          sísmica.
+        </p>
+      </div>
+
+    </div>
+
+    <div>
+
+      <div class="my-auto pl-20">
+        <img class=" animate__animated animate__fadeInRight w-10/12 rounded-lg " src='@/assets/images/ushuaia.jpg'
+          alt="turistas com snowmobile">
+      </div>
+
+      
+    </div>
+  </div>
+</template>
--- src/views/Historia.vue
+++ src/views/Historia.vue
@@ -1,5 +1,149 @@
 <template>
-    <h1 class="text-red-900"> Estou na Historia
-  
-    </h1>
-  </template>
+
+  <div class="my-14">
+    <img class=" animate__animated animate__fadeIn w-5/12 mx-auto rounded-lg " src='@/assets/images/historia.jpg'
+      alt="historia">
+  </div>
+
+  <section
+    class=" animate__animated animate__fadeInUp mx-80 grid gap-10 text-xl text-justify font-display font-semibold indent-16 border-r-2 pr-8">
+
+    <p>A Terra do Fogo foi habitada por povos indígenas desde cerca de onze mil anos atrás, procedentes do norte do
+      continente. Entre esses povos estavam os onas, os chonkóiunka, os alacalufos e os yagans. Embora tivessem culturas
+      particulares, eram em muito semelhantes entre si, tratando-se no geral de caçadores-coletores nômades que viviam
+      em habitações extremamente rústicas e vestiam-se sumariamente, quando não passavam completamente nus a despeito
+      das duras condições climáticas da ilha. Todos esses grupos se dividiam em pequenas comunidades autônomas,
+      frequentemente em guerra entre si e com costumes diferenciados, formando um verdadeiro mosaico cultural.
+
+      Na região de Ushuaia predominavam os yagans, estendendo sua presença até as duas entradas do canal de Beagle e o
+      cabo Horn. Possuíam uma rica mitologia e uma língua surpreendentemente complexa e sutil para uma cultura
+      paupérrima em todos os outros aspectos. Embora habitassem em terra, eram primariamente canoeiros nômades que se
+      alimentavam de mariscos ou caçavam guanacos com arco e flecha, passando pouco tempo em cada local para logo depois
+      levantarem acampamento e procurarem locais com alimentos mais abundantes. Disso deriva que sua concepção de mundo
+      era peculiar, já que o entendiam a partir da água e não da terra. Passavam a maior parte do tempo nus ou vestiam
+      apenas uma pele de animal atada sobre os ombros. Eram morenos, tinham uma estatura que não ultrapassava 1,6 m, sua
+      vida era curta e as dificuldades da sobrevivência eram extremas, sendo a fome muito comum. Não hesitavam em comer
+      carcaças semiapodrecidas de baleias e focas mortas que arribavam à praia. Apesar de terem uma colorida tradição
+      mitológica e mágica, preservada pelos seus xamãs, não tinham um conceito de Deus ou de um Criador.
+    </p>
+
+    <p>
+      A primeira viagem documentada do homem branco ao extremo sul do continente americano foi realizada por Fernão de
+      Magalhães em 1520, embora já tenha sido sugerido que o europeu já conhecia a região antes disso. De qualquer
+      maneira, foi Magalhães quem deu o nome de Terra do Fogo à região, tendo avistado fogueiras acesas pelos índios nas
+      margens do estreito que hoje leva seu nome. A bordo ia também o italiano Antonio Pigafetta, que desempenhou
+      importante papel científico escrevendo o primeiro léxico da linguagem da Patagônia, elaborado com o auxílio de um
+      indígena capturado. Outros navegadores depois dele se aventuraram pela Terra do Fogo, mas o primeiro que
+      seguramente permaneceu um longo tempo em terra foi Alonso de Camargo, que em 1541 invernou em uma baía fueguina,
+      possivelmente localizada na Ilha dos Estados ou na entrada do canal de Beagle.
+
+      Nesta altura todo o território fora reivindicado pela coroa espanhola, mas as incursões de exploradores e
+      corsários ingleses e holandeses fizeram com que a Espanha planejasse a fortificação e povoamento da região. Foram
+      enviadas várias naus com oficiais e colonos mas nenhuma logrou seu intento. Enquanto isso, a geografia fueguina ia
+      sendo cartografada com precisão cada vez maior. No século XVIII continuaram as viagens exploratórias,
+      acrescentando-se a participação francesa, e surgiram conflitos entre as potências europeias pela soberania da
+      região, registrando-se batalhas navais e diversos naufrágios, além de ser concebido o primeiro plano de
+      evangelização dos indígenas, que entretanto não se concretizou. Em 1771 Manuel Pando percorreu pela primeira vez
+      documentadamente o canal de Beagle e em 1788 a região foi estudada pela primeira vez por uma expedição científica,
+      a mando do italiano Antonio Malaspina, que estava a serviço da Espanha. Em 1790 foi realizada outra, capitaneada
+      por Juan José de Elizalde, a primeira de que participou um oficial argentino, Francisco José Cándido de Lasala.
+      Aparentemente nesta época já havia se estabelecido um comércio de peles e óleo dos lobos-marinhos existentes na
+      região.
+
+      Em 22 de maio de 1826 zarpou da Inglaterra uma expedição científica liderada pelo capitão Philip Parker King. O
+      capitão, contudo, por razões desconhecidas suicidou-se quando estava no estreito de Magalhães, e o comando foi
+      assumido pelo jovem imediato Robert FitzRoy. A viagem teve importantes consequências: nela foram capturados alguns
+      nativos fueguinos e foi explorada uma grande parte do canal de Beagle. Os nativos acabaram sendo levados para a
+      Inglaterra e lá educados. Dois deles, que receberam os nomes de Jeremy Buttom e Fuegia Basket, conseguiram aceitar
+      a educação e aprenderam a língua e os costumes ingleses. Sua presença na capital inglesa foi uma sensação, e
+      acabaram sendo apresentados à corte. Disso resultou o projeto de uma missão para evangelizar os nativos. A viagem
+      foi organizada e partiu em 27 de novembro de 1831, com FitzRoy ao comando do brigue HMS Beagle. Levava consigo o
+      mais tarde celebrado naturalista Charles Darwin e o missionário Richard Matthews, bem como os índios que
+      pretendiam devolver a seu povo. Enquanto o Beagle explorava os canais levantando sua cartografia e Darwin fazia
+      importantes observações científicas, Matthews foi deixado em terra, em uma pequena cabana, para iniciar seu
+      trabalho missionário. Com ele ficaram os índios, mas logo fugiram e retornaram ao estado selvagem. Quando o Beagle
+      retornou pouco depois, Matthews estava desolado, pois além da fuga dos seus pupilos outros índios ameaçavam sua
+      vida e haviam roubado tudo o que ele tinha consigo. Com isso, abandonou seu projeto e retornou para o Beagle.
+    </p>
+
+
+    <p>
+      Em 19 de junho de 1829 a Argentina, já independente, fez sua primeira afirmação de soberania sobre a Terra do
+      Fogo, criando uma comandância militar, sediada nas Ilhas Malvinas. Pouco mais tarde os ingleses ocuparam as ilhas,
+      dando margem a ideias de enviar novamente missionários anglicanos para a Terra do Fogo, mas as primeiras
+      tentativas, organizadas por Allen Francis Gardiner, fracassaram diante da hostilidade dos indígenas e da falta de
+      apoio oficial, acabando no martírio dos missionários. Enquanto isso a Argentina também fazia movimentos para a
+      povoação do extremo sul. Em 1852 o oficial Luis Piedra Buena com sua família ergueu uma casa na Ilha dos Estados,
+      cuja propriedade lhe foi outorgada pelo presidente Bartolomé Mitre em 1868, hasteando na ilha pela primeira vez a
+      bandeira argentina. No ano seguinte Waite Hockin Stirling, antigo colaborador de Gardiner, levantou uma missão na
+      ilha Navarino exatamente diante da futura Ushuaia, levando cabras e iniciando plantações a título de experiência.
+      Logo compreendeu que o local não era adequado, e transladou-se para o outro lado do canal, escolhendo a península
+      que fecha pela esquerda a baía de Ushuaia, onde poderia ficar mais próximo dos indígenas. Ali a partir de 18 de
+      janeiro de 1869 ergueu uma casa em um ponto elevado da península, cujo sítio é hoje um monumento nacional
+      argentino.
+
+      Embora Stirling contasse com a simpatia de alguns nativos, a quem ensinou os rudimentos do Evangelho e dos hábitos
+      europeus, muitos outros ainda constituíam uma constante ameaça. Seu trabalho foi interrompido no fim do ano,
+      quando foi designado bispo da diocese anglicana da América do Sul, devendo retornar à Inglaterra para sua
+      ordenação e empossamento no cargo. Outros missionários continuaram sua obra em Ushuaia, estabelecendo os inícios
+      de um povoamento que desde então foi ininterrupto. Em 30 de março de 1870 chegou ao local Thomas Bridges, que foi
+      o verdadeiro motor do povoamento inicial e estendeu a influência missionária por toda a região, paulatinamente
+      ganhando a confiança dos indígenas e reunindo-os em grande número, não sem encontrar ao mesmo tempo inúmeras
+      resistências e dificuldades de toda ordem. No ano seguinte foram batizados os primeiros indígenas, realizando-se
+      também os primeiros casamentos cristãos entre eles. Bridges também foi importante por ter composto o primeiro
+      dicionário da língua yagan e por ter realizado a primeira tradução de um texto bíblico para este idioma, uma
+      versão do Evangelho de São Lucas intitulada Gospl Luc Ecamanaci. Entretanto, a despeito de seus esforços para a
+      proteção dos índios, sua população decrescia dramaticamente, assolada por sucessivas epidemias trazidas pelo homem
+      branco, sem contar os enfrentamentos violentos com outros exploradores europeus que resultavam em inúmeras mortes.
+      Outros se entregavam à bebida ou vendiam suas filhas para se prostituírem para os marinheiros. Em meados da década
+      de 1880 Bridges acusava a existência de somente 387 índios yagan em todo o arquipélago.
+
+      Depois da partilha da Terra do Fogo entre Chile e Argentina em 1881, definindo seus limites, em 1883 o governo
+      argentino decidiu enviar a "Divisão Expedicionária ao Atlântico Sul", sob o comando do coronel Augusto Lasserre, a
+      fim de ali estabelecer subprefeituras. Sete navios de guerra foram designados para a missão, que aportou primeiro
+      na Ilha dos Estados e em 1884 entrou na Baía de Ushuaia, apenas com quatro naus. Em 12 de outubro de 1884 foi
+      enfim içada a bandeira argentina e criada a subprefeitura local, com Alejandro Virasoro y Calvo ocupando a
+      primeira magistratura, auxiliado por 44 oficiais subalternos. A data constitui a fundação oficial da cidade.
+      Poucos dias depois, em 16 de outubro, foi criado o Território da Terra do Fogo, do qual Ushuaia foi escolhida
+      naturalmente como a capital, tendo Félix Paz como governador, e, no ano seguinte, como capital de um dos três
+      Departamentos em que o Território foi subdividido. Paz transferiu em 1887 a subprefeitura para a baía Bom Sucesso,
+      e logo depois para a baía Tétis, possivelmente para não confundir as administrações, mas a experiência não deu
+      certo e Ushuaia voltou a acumular todas as funções em 1896.
+
+      Enquanto os índios desapareciam quase completamente, aumentava devagar o número de colonos brancos, chegando
+      grupos de espanhóis, portugueses, argentinos e italianos, bem como garimpeiros do ouro que fora descoberto no
+      norte da ilha e que acabavam sempre gerando conflitos. Esses primeiros colonos brancos de Ushuaia também tiveram
+      de lutar por sua própria sobrevivência, e vários abandonaram a comunidade, que sempre estava à míngua de apoio
+      oficial e dependia em larga medida do intermédio da florescente cidade chilena de Punta Arenas para muitos
+      serviços e abastecimento. Um relato de 1893 do inspetor de escolas Raúl Díaz fez um retrato patético da
+      situação do povoado:
+
+      Dezesseis casas sem nenhuma comodidade formam ruelas visíveis unicamente devido à distância que as separam; cinco
+      armazéns de pouca importância, uma escola com poucos meninos que quase sempre está fechada por falta de pessoal,
+      uma serraria a vapor, boa obra do tenente-coronel Godoy, uma formosa e cômoda baía que os barcos frequentam só
+      muito raramente, uma Missão anglicana na frente e por fim oitenta habitantes que vivem bocejando, isolados, a 600
+      léguas de Buenos Aires.
+      Como Ushuaia agora era capital de um Território, aos poucos iam aparecendo algumas melhorias e a sociedade se
+      organizava. No final do século XIX a escola foi reforçada com novos professores e foram criados o juizado de paz,
+      o registro civil, o serviço médico, o correio, um destacamento policial, um pequeno cárcere, enquanto eram
+      fundadas algumas estâncias na zona rural, além de ser construído o prédio do cabildo e se instituírem outras
+      repartições públicas. Também sediou diversas expedições científicas nesta época, estrangeiras e nacionais, foi
+      elaborada primeira planta urbana para orientar o futuro crescimento da cidade e começou a ser implantado um
+      projeto de estabelecimento de uma colônia penal para fomentar o povoamento. Em 1895 havia uma população de 477
+      pessoas, com 225 delas vivendo na zona urbana, excluídos os índios.
+
+      O trabalho das missões inglesas prosseguia com o beneplácito das autoridades argentinas, e atraíam indígenas de
+      outras etnias, como os onas, que encontraram nelas um refúgio temporário até que as doenças também os dizimassem.
+      As missões por outro lado tiveram uma importância econômica introduzindo o gado bovino, ovino e caprino na região,
+      e iniciando a exploração e beneficiamento da abundante madeira da ilha. Bridges faleceu em 1898 em Buenos Aires, e
+      sua obra foi continuada por seus filhos na sua Estância Harberton e outros seguidores na antiga sede na península,
+      até que quase já não restavam mais índios, sendo a missão original encerrada em 1910. Os poucos índios
+      remanescentes foram transferidos para outro estabelecimento na ilha Navarino, sob os cuidados do missionário John
+      Williams, onde permaneceram até 1913. A constante presença dos anglicanos acabou por despertar a atenção dos
+      eclesiásticos católicos argentinos. Os salesianos assumiram o encargo e enviaram para lá seu primeiro missionário
+      em 1897, o padre Ramón Lista. Foi edificada uma capela em 1898, da qual foi nomeado pároco o padre Juan Fossatti.
+    </p>
+
+  </section>
+
+</template>
