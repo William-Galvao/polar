36e1f60d4d5983d594c758568995af5c755b4981
sanossuke <andreygustavo.oliveirasilva@gmail.com>
2022-09-26 19:42:00 -0300

    finished 2

--- src/views/Geografia.vue
+++ src/views/Geografia.vue
@@ -3,7 +3,7 @@
 
     <div class="pr-20">
       <p
-        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-16 ">
         O nome Ushuaia é uma combinação de duas palavras do idioma yagan: ushu, um termo de difícil tradução com o
         significado de algo que penetra amplamente em direção ao oeste, e aia, baía. O resultado pode ser interpretado
         como "baía que penetra fundo terra adentro na direção oeste". A palavra Ushuaia apareceu por escrito pela
@@ -12,7 +12,7 @@
       </p>
 
       <p
-        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+        class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-16 ">
         Ushuaia encontra-se nas coordenadas 54° 48' 57" S 68° 19' 04" O, a 3 mil km de Buenos Aires, a capital
         argentina. Está a sudoeste da Ilha Grande da Terra do Fogo, aninhada entre as encostas do cerro Martial e do
         monte Olívia e debruçando-se sobre a Baía de Ushuaia, no limite setentrional do canal de Beagle. É a capital
@@ -22,8 +22,24 @@
         Estados e alguns ilhotes no canal de Beagle.
       </p>
       <div>
-        <p
-          class=" animate__animated animate__fadeInLeft text-xl text-justify font-display font-semibold indent-16 border-l-4 pl-10 ">
+       
+      </div>
+
+    </div>
+
+    <div>
+
+      <div class="my-auto pl-20">
+        <img class=" animate__animated animate__fadeInRight w-10/12 rounded-lg " src='@/assets/images/ushuaia.jpg'
+          alt="turistas com snowmobile">
+      </div>
+
+      
+    </div>
+  </div>
+
+  <p
+          class=" animate__animated animate__fadeInUp mx-20 pt-8 grid gap-10 text-xl text-justify font-display font-semibold indent-16 border-r-2 pr-8 ">
           A cidade ocupa um dos três ambientes geográficos distintos encontrados na ilha, caracterizando-se pela
           presença
           dos últimos trechos da cordilheira dos Andes, que na região se estende de oeste a leste e se subdivide em
@@ -45,18 +61,5 @@
           de Ushuaia está próxima da falha de Magalhães-Fagnano, que atravessa a ilha de oeste a leste e tem atividade
           sísmica.
         </p>
-      </div>
 
-    </div>
-
-    <div>
-
-      <div class="my-auto pl-20">
-        <img class=" animate__animated animate__fadeInRight w-10/12 rounded-lg " src='@/assets/images/ushuaia.jpg'
-          alt="turistas com snowmobile">
-      </div>
-
-      
-    </div>
-  </div>
 </template>
