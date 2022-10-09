a84b5b052ab1c8614300e9e71cd1e8f5df6895bc
sanossuke <andreygustavo.oliveirasilva@gmail.com>
2022-10-09 17:03:50 -0300

    Andrey

--- src/main.js
+++ src/main.js
@@ -2,5 +2,14 @@ import { createApp } from "vue";
 import "@/style.css";
 import App from "@/App.vue";
 import router from "@/routes/index";
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+import {library} from '@fortawesome/fontawesome-svg-core';
+import {fas} from '@fortawesome/free-solid-svg-icons';
 
-createApp(App).use(router).mount("#app");
+library.add(fas);
+
+
+createApp(App)
+.use(router)
+.component('fa',FontAwesomeIcon)
+.mount("#app");
